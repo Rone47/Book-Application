c8a6bba46a82ccd55c1ebd30ec207be4deefdbb4
Rone47 <ryanbyrone47@gmail.com>
2025-05-03 02:30:16 +0800

    commit

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native'
-
+import { View, Image, Text, TextInput } from "react-native";
+import styles from "../../assets/styles/login.styles";
+import { useState } from "react";
+import {Ionicons} from '@expo/vector-icons';
+import COLORS from "../../constants/colors";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {};
+
   return (
-    <View>
-      <Text> Login Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.topIllustration}>
+        <Image
+          source={require("../../assets/images/i.png")}
+          style={styles.illustrationImage}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.formContainer}>
+          {/* Email */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="mail-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your email"
+                placeholderTextColor={COLORS.placeholderText}
+                value={email}
+                onChange={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
