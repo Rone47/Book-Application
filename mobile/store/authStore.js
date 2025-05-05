12b9d9e13bcc4a0190a01b0a140f7656519d4126
Rone47 <ryanbyrone47@gmail.com>
2025-05-05 21:32:43 +0300

    add

--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -14,18 +14,15 @@ export const useAuthStore = create((set) => ({
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          username,
-          email,
-          password,
-        }),
+        body: JSON.stringify({ username, email, password }),
       });
+  
       const data = await response.json();
-
+  
       if (!response.ok) throw new Error(data.message || "Something went wrong");
       await AsyncStorage.setItem("user", JSON.stringify(data.user));
       await AsyncStorage.setItem("token", data.token);
-
+  
       set({ token: data.token, user: data.user, isLoading: false });
       return { success: true };
     } catch (error) {
