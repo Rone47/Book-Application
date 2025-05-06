1e7e9d95496bffa78bbf5efcc7982cc8b95d74b3
Rone47 <ryanbyrone47@gmail.com>
2025-05-06 14:50:50 +0300

    add

--- backend/src/lib/cron.js
+++ backend/src/lib/cron.js
@@ -4,8 +4,8 @@ import https from "https";
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("Get request sent successfully");
-      else console.log("Get requst failed", res.statusCode);
+      if (res.statusCode === 200) console.log("GET request sent successfully");
+      else console.log("GET requst failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sendng request", e));
 });
