b396dac8634c24897a5eb27f81423bc4b50877c8
Rone47 <ryanbyrone47@gmail.com>
2025-05-15 20:08:45 +0300

    addd

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,7 +1,9 @@
-import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
+import { View, Text, TouchableOpacity, FlatList  } from "react-native";
 import { useEffect, useState } from "react";
 import { useAuthStore } from "../../store/authStore";
+import {Image} from 'expo-image'
 import styles from "../../assets/styles/home.styles";
+import { API_URL } from "../../constants/api";
 
 export default function Home() {
   const { token } = useAuthStore();
@@ -11,7 +13,29 @@ export default function Home() {
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
 
-  const fetchBooks = async (pageNum = 1, refresh = false) => {};
+  const fetchBooks = async (pageNum = 1, refresh = false) => {
+    try {
+      if (refresh) setRefreshing(true);
+      else if (pageNum === 1) setLoading(true);
+
+      const response = await fetch(`${API_URL}/books?page=${pageNum}&limit=5`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      const data = await response.json();
+      if (!response.ok)
+        throw new Error(data.message || "Failed to fetch books");
+
+      setBooks((prevBooks) => [...prevBooks, ...data.books]);
+      setHasMore(pageNum < data.totalPages);
+      setPage(pageNum);
+    } catch (error) {
+      console.log("Error fetching Books", error);
+    } finally {
+      if (refresh) setRefreshing(false);
+      else setLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchBooks();
@@ -32,7 +56,11 @@ export default function Home() {
       </View>
 
       <View style={styles.bookImageContainer}>
-        <Image source={item.image} style={styles.bookImage} contentFit='cover'/>
+        <Image
+          source={item.image}
+          style={styles.bookImage}
+          contentFit="cover"
+        />
       </View>
     </View>
   );
