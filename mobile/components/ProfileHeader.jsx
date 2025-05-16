37211004d01e5e2a117a5f4ca843164d4bc1633b
Rone47 <ryanbyrone47@gmail.com>
2025-05-16 21:03:48 +0300

    add

--- mobile/components/ProfileHeader.jsx
+++ mobile/components/ProfileHeader.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import { useAuthStore } from "../store/authStore";
+import styles from "../assets/styles/create.styles";
+import { Image } from "expo-image";
 
 export default function ProfileHeader() {
+  const { user } = useAuthStore();
   return (
-    <View>
-      <Text>ProfileHeader</Text>
+    <View style={styles.profile.ProfileHeader}>
+      <Image source={{uri: user.profileImage}} style={styles.previewImage}/>
+      <View style={styles.profileInfo}>
+        <Text style={styles.username}>{user.username}</Text>
+      </View>
     </View>
-  )
+  );
 }
