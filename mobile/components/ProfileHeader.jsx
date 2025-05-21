5b83cee2c864c5cf5dd91d71d8e3321583509948
Rone47 <ryanbyrone47@gmail.com>
2025-05-21 19:34:55 +0300

    add

--- mobile/components/ProfileHeader.jsx
+++ mobile/components/ProfileHeader.jsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import { useAuthStore } from "../store/authStore";
 import styles from "../assets/styles/create.styles";
 import { Image } from "expo-image";
+import { formatMemberSince } from "../lib/utils";
 
 export default function ProfileHeader() {
   const { user } = useAuthStore();
@@ -10,6 +11,8 @@ export default function ProfileHeader() {
       <Image source={{uri: user.profileImage}} style={styles.previewImage}/>
       <View style={styles.profileInfo}>
         <Text style={styles.username}>{user.username}</Text>
+        <Text style={styles.email}>{user.email}</Text>
+        <Text style={styles.memberSince}>Joined {formatMemberSince(createdAt)}</Text>
       </View>
     </View>
   );
