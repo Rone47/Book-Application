dd89f0a3ac1713a25072c53bf52f2574bd047933
Rone47 <ryanbyrone47@gmail.com>
2025-05-21 21:29:10 +0300

    add

--- mobile/app/(tabs)/profile.jsx
+++ mobile/app/(tabs)/profile.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { View, Alert, Text } from "react-native";
+import {
+  View,
+  Alert,
+  Text,
+  FlatList,
+  TouchableNativeFeedbackComponent,
+} from "react-native";
 import { useRouter } from "expo-router";
 import { API_URL } from "../../constants/api";
 import { useAuthStore } from "../../store/authStore";
@@ -7,6 +13,8 @@ import { useEffect } from "react";
 import styles from "../../assets/styles/create.styles";
 import ProfileHeader from "../../components/ProfileHeader";
 import LogoutButton from "../../components/LogoutButton";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
 
 const [books, setBooks] = useState([]);
 const [isLoading, setIsLoading] = useState(true);
@@ -42,13 +50,35 @@ useEffect(() => {
 export default function profile() {
   return (
     <View style={styles.container}>
-      <ProfileHeader/>
-      <LogoutButton/>
+      <ProfileHeader />
+      <LogoutButton />
 
       <View style={styles.booksHeader}>
         <Text style={styles.booksTitle}>Your Reccomandation</Text>
         <Text style={styles.booksCount}>{books.length}</Text>
       </View>
+      <FlatList
+        data={books}
+        renderItem={renderBookItem}
+        keyExtractor={(item) => item._id}
+        showsVerticalScrollIndicator={styles.booksList}
+        ListEmptyComponent={
+          <View style={styles.emptyContainer}>
+            <Ionicons
+              name="book-outline"
+              size={50}
+              color={COLORS.textSecondary}
+            />
+            <Text style={styles.emptyText}>No reccomandations yet</Text>
+            <TouchableNativeFeedbackComponent
+              style={styles.addButton}
+              onPress={() => router.push("/create")}
+            >
+              <Text style={styles.addButtonText}>Add Your First Book</Text>
+            </TouchableNativeFeedbackComponent>
+          </View>
+        }
+      />
     </View>
   );
 }
